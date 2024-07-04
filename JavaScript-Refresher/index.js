998b98be4b6cf8e8728cd164e96b3bf242b9d872
junaidhashim6 <junaidhashim6@gmail.com>
2024-07-04 15:36:14 +0500

    modern objects

--- JavaScript-Refresher/index.js
+++ JavaScript-Refresher/index.js
@@ -1,16 +1,27 @@
-const name = "junaid";
-const age = 27;
-const hasHobbies = true;
+// const name = "junaid";
+// const age = 27;
+// const hasHobbies = true;
 
-const UserDetails = (userName, age, hobbies) => {
-  return (
-    "Name of the user is " +
-    userName +
-    " Age of the user is " +
-    age +
-    " Hobbies: " +
-    hobbies
-  );
+// const UserDetails = (userName, age, hobbies) => {
+//   return (
+//     "Name of the user is " +
+//     userName +
+//     " Age of the user is " +
+//     age +
+//     " Hobbies: " +
+//     hobbies
+//   );
+// };
+
+// console.log(UserDetails(name, age, hasHobbies));
+
+// Objects
+
+const person = {
+  name: "john",
+  age: 25,
+  greet() {
+    console.log("hello my name is " + this.name);
+  },
 };
-
-console.log(UserDetails(name, age, hasHobbies));
+person.greet();
